1679944114a772c3d625a9c24a3311b882d4880b
Fabio Tessaro <36379568+FabioTessaro@users.noreply.github.com>
2026-09-05 10:48:43 +0100

    Add installation script for Drive Disk Settings

---
+++ tcf_core/install.py
@@ -0,0 +1,14 @@
+import frappe
+
+def after_install():
+    configure_drive_storage()
+
+def configure_drive_storage():
+    settings = frappe.get_single("Drive Disk Settings")
+    settings.enabled = 1
+    settings.aws_key = frappe.conf.get("seaweedfs_access_key")
+    settings.aws_secret = frappe.conf.get("seaweedfs_secret_key")
+    settings.bucket = frappe.conf.get("seaweedfs_bucket")
+    settings.endpoint_url = "drive-vault"
+    settings.signature_version = "s3v4"
+    settings.save()
